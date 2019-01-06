5023f402f632495578adbfee7c380604ed3bd2d2
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-01-06 15:38:16 +0530

    Updates

---
+++ precache-manifest.353cd1c9c6f699c93562859b986f2c63.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ba39e07d1f8d98b362b9b65d6544538",
+    "url": "/js30/static/media/hihat.4ba39e07.wav"
+  },
+  {
+    "revision": "5fb59f5db9b050fd714c",
+    "url": "/js30/static/css/main.760018c4.chunk.css"
+  },
+  {
+    "revision": "572ed39d3a51fff664d47d7b7b508292",
+    "url": "/js30/static/media/tom.572ed39d.wav"
+  },
+  {
+    "revision": "936c658f3df00fabb705",
+    "url": "/js30/static/js/1.936c658f.chunk.js"
+  },
+  {
+    "revision": "49fab0eaa2d09950bf8a",
+    "url": "/js30/static/js/runtime~main.49fab0ea.js"
+  },
+  {
+    "revision": "a6e86dd7c2d3dee1ec0ad70c0d69df36",
+    "url": "/js30/static/media/clap.a6e86dd7.wav"
+  },
+  {
+    "revision": "550be808a1e25f5b4db6b66350270316",
+    "url": "/js30/static/media/boom.550be808.wav"
+  },
+  {
+    "revision": "5fb59f5db9b050fd714c",
+    "url": "/js30/static/js/main.5fb59f5d.chunk.js"
+  },
+  {
+    "revision": "4dd585951cfd8c9f78cf3241ec4bbb7d",
+    "url": "/js30/static/media/kick.4dd58595.wav"
+  },
+  {
+    "revision": "a62db465466a58a60a40c4466d7730e5",
+    "url": "/js30/static/media/openhat.a62db465.wav"
+  },
+  {
+    "revision": "b0d70ec54db9b098b9358f98d131c0f9",
+    "url": "/js30/static/media/ride.b0d70ec5.wav"
+  },
+  {
+    "revision": "80c4129b4b6b8810a079d6c311bf1d4b",
+    "url": "/js30/static/media/snare.80c4129b.wav"
+  },
+  {
+    "revision": "2a6bb6f58bb711ed7ea121b225dcc92d",
+    "url": "/js30/static/media/tink.2a6bb6f5.wav"
+  },
+  {
+    "revision": "936c658f3df00fabb705",
+    "url": "/js30/static/css/1.19d86d5d.chunk.css"
+  },
+  {
+    "revision": "b98b53b6dc2063753b5b0d8a4d9bfd5f",
+    "url": "/js30/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/js30/precache-manifest.ac5b30ecc12e90dedd148dfb6cb84d1b.js"
+  "/js30/precache-manifest.353cd1c9c6f699c93562859b986f2c63.js"
 );
 
 workbox.clientsClaim();
